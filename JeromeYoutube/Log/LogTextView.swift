67e9d8dc5c399c5e0d7c10e0dcc3b3070e88f509
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-09-10 20:25:43 +0800

    1.添加 Log 檔案。

---
+++ JeromeYoutube/Log/LogTextView.swift
@@ -0,0 +1,31 @@
+//
+//  LogTextView.swift
+//  TaiChungWeather
+//
+//  Created by JEROME on 2019/7/29.
+//  Copyright © 2019 JEROME. All rights reserved.
+//
+
+import UIKit
+
+class LogTextView: UITextView {
+  override init(frame: CGRect, textContainer: NSTextContainer?) {
+    super.init(frame: frame, textContainer: textContainer)
+
+    setupStyle()
+  }
+
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func hitTest(_: CGPoint, with _: UIEvent?) -> UIView? {
+    return nil
+  }
+
+  private func setupStyle() {
+    backgroundColor = .white
+    alpha = 0.7
+    textColor = .black
+  }
+}
