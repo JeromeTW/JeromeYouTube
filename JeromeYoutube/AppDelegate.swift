980444e0271078c7562d6f2d31cff2e8450f0c54
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-09-12 16:11:10 +0800

    1.添加 Core Data 相關程式。2.實作 Video Category 和 Video 兩張資料表。

--- JeromeYoutube/AppDelegate.swift
+++ JeromeYoutube/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import AVFoundation
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -59,6 +60,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window.rootViewController = tabBarController
     window.makeKeyAndVisible()
   }
+  
+  func applicationWillTerminate(_: UIApplication) {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Saves changes in the application's managed object context before the application terminates.
+    persistentContainer.saveContext()
+  }
+  
+  // MARK: - Core Data stack
+  lazy var persistentContainer: NSPersistentContainer = {
+    /*
+     The persistent container for the application. This implementation
+     creates and returns a container, having loaded the store for the
+     application to it. This property is optional since there are legitimate
+     error conditions that could cause the creation of the store to fail.
+     */
+    let container = NSPersistentContainer(name: "Video")
+    container.loadPersistentStores(completionHandler: { _, error in
+      if let error = error as NSError? {
+        // Replace this implementation with code to handle the error appropriately.
+        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        
+        /*
+         Typical reasons for an error here include:
+         * The parent directory does not exist, cannot be created, or disallows writing.
+         * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+         * The device is out of space.
+         * The store could not be migrated to the current model version.
+         Check the error message to determine what the actual problem was.
+         */
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    return container
+  }()
+  
+  lazy var viewContext: NSManagedObjectContext = {
+    persistentContainer.viewContext
+  }()
 }
 
 // MARK: - Log
---
+++ JeromeYoutube/Core Data/CoreDataConnect.swift
@@ -0,0 +1,170 @@
+//
+//  CoreDataConnect.swift
+//
+//  Created by JEROME on 2016/10/18.
+//  Copyright © 2016年 JEROME. All rights reserved.
+//
+
+import CoreData
+import Foundation
+
+class CoreDataConnect {
+  var myContext: NSManagedObjectContext!
+
+  init(context: NSManagedObjectContext) {
+    myContext = context
+  }
+
+  // insert
+  func insert(_ myEntityName: String, attributeInfo: [String: Any]) -> Bool {
+    let insetData = NSEntityDescription.insertNewObject(forEntityName: myEntityName, into: myContext)
+
+    for (key, value) in attributeInfo {
+      insetData.setValue(value, forKey: key)
+    }
+
+    do {
+      try myContext.save()
+
+      return true
+    } catch {
+      fatalError("\(error)")
+    }
+  }
+
+  // retrieve
+  func retrieve(_ myEntityName: String, predicate: NSPredicate?, sort: [[String: Bool]]?, limit: Int?) -> [NSManagedObject]? {
+    let request = NSFetchRequest<NSFetchRequestResult>(entityName: myEntityName)
+
+    // predicate
+    request.predicate = predicate
+
+    // sort
+    if let mySort = sort {
+      var sortArr: [NSSortDescriptor] = []
+      for sortCond in mySort {
+        for (k, v) in sortCond {
+          sortArr.append(NSSortDescriptor(key: k, ascending: v))
+        }
+      }
+
+      request.sortDescriptors = sortArr
+    }
+
+    // limit
+    if let limitNumber = limit {
+      request.fetchLimit = limitNumber
+    }
+
+    do {
+      return try myContext.fetch(request) as? [NSManagedObject]
+
+    } catch {
+      fatalError("\(error)")
+    }
+  }
+
+  // update
+  func update(_ myEntityName: String, predicate: NSPredicate?, attributeInfo: [String: String]) -> Bool {
+    if let results = self.retrieve(myEntityName, predicate: predicate, sort: nil, limit: nil) {
+      for result in results {
+        for (key, value) in attributeInfo {
+          let t = result.entity.attributesByName[key]?.attributeType
+
+          if t == .integer16AttributeType || t == .integer32AttributeType || t == .integer64AttributeType {
+            result.setValue(Int(value), forKey: key)
+          } else if t == .doubleAttributeType || t == .floatAttributeType {
+            result.setValue(Double(value), forKey: key)
+          } else if t == .booleanAttributeType {
+            result.setValue(value == "true" ? true : false, forKey: key)
+          } else {
+            result.setValue(value, forKey: key)
+          }
+        }
+      }
+
+      do {
+        try myContext.save()
+
+        return true
+      } catch {
+        fatalError("\(error)")
+      }
+    }
+
+    return false
+  }
+
+  // delete
+  func delete(_ myEntityName: String, predicate: NSPredicate?) -> Bool {
+    if let results = self.retrieve(myEntityName, predicate: predicate, sort: nil, limit: nil) {
+      for result in results {
+        myContext.delete(result)
+      }
+
+      do {
+        try myContext.save()
+
+        return true
+      } catch {
+        fatalError("\(error)")
+      }
+    }
+
+    return false
+  }
+
+  func getCount(_ myEntityName: String, predicate: NSPredicate?) -> Int {
+    var count = 0
+    let request = NSFetchRequest<NSNumber>(entityName: myEntityName)
+
+    // predicate
+    request.predicate = predicate
+    request.resultType = .countResultType
+
+    do {
+      let countResult = try myContext.fetch(request)
+      // 获取数量
+      count = countResult.first!.intValue
+    } catch let error as NSError {
+      assertionFailure("Could not fetch \(error), \(error.userInfo)")
+    }
+    return count
+  }
+
+  public func getFRC(_ myEntityName: String, predicate: NSPredicate?, sort: [[String: Bool]]?, limit: Int?) -> NSFetchedResultsController<NSFetchRequestResult> {
+    let request = NSFetchRequest<NSFetchRequestResult>(entityName: myEntityName)
+
+    // predicate
+    request.predicate = predicate
+
+    // sort
+    if let mySort = sort {
+      var sortArr: [NSSortDescriptor] = []
+      for sortCond in mySort {
+        for (k, v) in sortCond {
+          sortArr.append(NSSortDescriptor(key: k, ascending: v))
+        }
+      }
+
+      request.sortDescriptors = sortArr
+    }
+
+    // limit
+    if let limitNumber = limit {
+      request.fetchLimit = limitNumber
+    }
+    let fetchedResultsController = NSFetchedResultsController(fetchRequest: request,
+                                                              managedObjectContext: myContext,
+                                                              sectionNameKeyPath: nil,
+                                                              cacheName: nil)
+    do {
+      try fetchedResultsController.performFetch()
+    } catch {
+      let fetchError = error as NSError
+      printLog("\(fetchError), \(fetchError.userInfo)", level: .error)
+    }
+
+    return fetchedResultsController
+  }
+}
