85b0093b227221d71d27d91831407d9dfa5cc122
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-09-13 17:54:01 +0800

    1.添加常用的工具檔案。

---
+++ JeromeYoutube/Utils/Base/UI/JeromeNavigationBar.swift
@@ -0,0 +1,19 @@
+//
+//  JeromeNavigationBar.swift
+//  JeromeYoutube
+//
+//  Created by JEROME on 2019/9/13.
+//  Copyright © 2019 jerome. All rights reserved.
+//
+
+import UIKit
+
+class JeromeNavigationBar: UIView {
+  
+}
+
+extension UIViewController {
+  func addJeromeNavigationBar() {
+    
+  }
+}
