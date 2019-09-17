7add95c6b943afb148a7c829e5da2db38efda94d
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-09-17 11:26:58 +0800

    1.刪除多餘的檔案。2. 實作 HasJeromeNavigationBar

--- JeromeYoutube/Utils/Base/UI/JeromeNavigationBar.swift
+++ JeromeYoutube/Utils/Base/UI/JeromeNavigationBar.swift
@@ -17,3 +17,44 @@ extension UIViewController {
     
   }
 }
+
+protocol HasJeromeNavigationBar: UIViewController {
+  var topView: UIView { get set }
+  var statusView: UIView { get set }
+  var navagationView: UIView { get set }
+  var statusViewHeightConstraint: NSLayoutConstraint { get set }
+  var navagationViewHeightConstraint: NSLayoutConstraint { get set }
+  var observer: NSObjectProtocol? { get set }
+  
+  func setupSatusBarHeightChangedObserver()
+  func removeSatusBarHeightChangedObserver()
+  func updateTopView()
+}
+
+extension HasJeromeNavigationBar {
+  func setupSatusBarHeightChangedObserver() {
+    observer = NotificationCenter.default.addObserver(forName: UIApplication.willChangeStatusBarFrameNotification, object: nil, queue: nil) { [weak self] _ in
+      DispatchQueue.main.async {
+        guard let self = self else {
+          return
+        }
+        let statusHeight = UIApplication.shared.statusBarFrame.size.height
+        self.statusViewHeightConstraint.constant = statusHeight
+      }
+    }
+  }
+  
+  func updateTopView() {
+    let statusHeight = UIApplication.shared.statusBarFrame.size.height
+    print("statusHeight=\(statusHeight)")
+    statusViewHeightConstraint.constant = statusHeight
+    
+    
+  }
+  
+  func removeSatusBarHeightChangedObserver() {
+    if let observer = observer {
+      NotificationCenter.default.removeObserver(observer)
+    }
+  }
+}
