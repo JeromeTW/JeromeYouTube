f78e0cc1595240655f01e2a65459f0e52b2167f9
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-09-18 09:57:01 +0800

    1.刪除多餘的程式。

--- JeromeYoutube/JeromeYoutubePlayerVC.swift
+++ JeromeYoutube/JeromeYoutubePlayerVC.swift
@@ -11,30 +11,9 @@ import XCDYouTubeKit
 
 class JeromeYoutubePlayerVC: XCDYouTubeVideoPlayerViewController {
   
-  private let commandCenter = MPRemoteCommandCenter.shared()
-  private let youtubeClient = XCDYouTubeClient(languageIdentifier: "zh")
-  private var youtubeID: String
-  private var streamURL: URL?
   private var observer: NSObjectProtocol?
-  private var playingItem: AVPlayerItem? {
-    guard let streamURL = streamURL else {
-      return nil
-    }
-    return AVPlayerItem(url: streamURL)
-  }
-  
-  override init(videoIdentifier: String?) {
-    youtubeID = videoIdentifier!
-    super.init(videoIdentifier: videoIdentifier)
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
-    setupYoutubeClient()
     observer = NotificationCenter.default.addObserver(forName: NSNotification.Name.MPMoviePlayerPlaybackDidFinish, object: nil, queue: nil) { _ in
       YoutubePlayer.shared.youtubePlayerVC = nil
     }
@@ -44,88 +23,6 @@ class JeromeYoutubePlayerVC: XCDYouTubeVideoPlayerViewController {
     if let observer = observer {
       NotificationCenter.default.removeObserver(observer)
     }
-  }
-  
-  private func setupYoutubeClient() {
-    youtubeClient.getVideoWithIdentifier(youtubeID) { [weak self] video, error in
-      guard let self = self else {
-        return
-      }
-      guard error == nil else {
-        logger.log(error!)
-        return
-      }
-      guard let video = video else {
-        assertionFailure()
-        return
-      }
-      let streamURLs = video.streamURLs
-      if let tempStreamURL = (streamURLs[XCDYouTubeVideoQualityHTTPLiveStreaming] ?? streamURLs[YouTubeVideoQuality.hd720] ?? streamURLs[YouTubeVideoQuality.medium360] ?? streamURLs[YouTubeVideoQuality.small240]) {
-        self.streamURL = tempStreamURL
-        self.setupRemoteCommandCenter()
-        self.setupNowPlayingInfo(video: video)
-      }
-    }
-  }
-  
-  private func setupRemoteCommandCenter() {
-    commandCenter.pauseCommand.isEnabled = true
-    commandCenter.playCommand.isEnabled = true
-    
-    commandCenter.playCommand.addTarget { [weak self] _ in
-      guard let self = self else { return .commandFailed }
-      guard let player = self.moviePlayer else { return .commandFailed }
-      guard player.playbackState != .playing else {
-        return .success
-      }
-      player.play()
-      guard player.playbackState == .playing else {
-        return .commandFailed
-      }
-      return .success
-    }
-    
-    commandCenter.pauseCommand.addTarget { [weak self] _ -> MPRemoteCommandHandlerStatus in
-      guard let self = self else { return .commandFailed }
-      guard let player = self.moviePlayer else { return .commandFailed }
-      guard player.playbackState == .playing else {
-        return .success
-      }
-      player.pause()
-      guard player.playbackState == .paused else {
-        return .commandFailed
-      }
-      return .success
-    }
-  }
-  
-  func setupNowPlayingInfo(video: XCDYouTubeVideo) {
-    DispatchQueue.main.asyncAfter(deadline: .now()) {   // 要放在main thread才能更新remote center UI, 延後一秒，等songItem 準備好
-      MPNowPlayingInfoCenter.default().nowPlayingInfo = [
-        MPMediaItemPropertyTitle: video.title,
-        MPMediaItemPropertyPlaybackDuration: video.duration
-      ]
-      
-      func setMPMediaItemPropertyArtwork(image: UIImage) {
-        let artwork = MPMediaItemArtwork.init(boundsSize: image.size, requestHandler: { _ -> UIImage in
-          return image
-        })
-        MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPMediaItemPropertyArtwork] = artwork
-      }
-      // TODO： MPNowPlayingInfoPropertyElapsedPlaybackTime: NSNumber(value: CMTimeGetSeconds(songItem.currentTime()))
-      if let thumbnailURL = video.thumbnailURL {
-        ImageLoader.shared.imageByURL(thumbnailURL, completionHandler: { image, _ in
-          guard let image = image else {
-            logger.log("No Image", level: .debug)
-            return
-          }
-          setMPMediaItemPropertyArtwork(image: image)
-        })
-      } else {
-        // 無縮圖用 soso 美照
-        let image = UIImage(named: "Soso")!
-        setMPMediaItemPropertyArtwork(image: image)
-      }
-    }
+    logger.log("\(self.className) deinit")
   }
 }
